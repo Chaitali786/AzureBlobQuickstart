16320b62b6b9b19f32fa0c1f6cf4f0a3ba3655d6
ChaitaliDeore <chaitalideore@gmail.com>
2024-06-10 21:07:45 +0200

    Database code remobed

--- AzureBlobQuickStart/deployment.py
+++ AzureBlobQuickStart/deployment.py
@@ -34,14 +34,14 @@ CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split('')}
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
                                                                                      
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": CONNECTION_STR['dbname'],
-        "HOST": CONNECTION_STR['host'],
-        "USER": CONNECTION_STR['user'],
-        "PASSWORD": CONNECTION_STR['password'],
-                   }
-    }
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.postgresql",
+   #     "NAME": CONNECTION_STR['dbname'],
+    #    "HOST": CONNECTION_STR['host'],
+     #   "USER": CONNECTION_STR['user'],
+      #  "PASSWORD": CONNECTION_STR['password'],
+       #            }
+    #}
 
 
