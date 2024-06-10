d487dc8325472e07df55b5d79863f8da65241955
ChaitaliDeore <chaitalideore@gmail.com>
2024-06-10 21:28:41 +0200

    settings.py updated

--- AzureBlobQuickStart/settings.py
+++ AzureBlobQuickStart/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-
+AZURE_STORAGE_CONNECTION_STRING = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -28,7 +28,7 @@ SECRET_KEY = 'MY_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blobstoragecontainer.azurewebsites.net/']
 
 
 # Application definition
